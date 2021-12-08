f9d6c603de94a08c95280ae655e250a596571f04
Barry Moore <chiroptical@gmail.com>
2021-12-07 20:43:54 -0500

    Get ready for day 8

---
+++ src/day8.rs
@@ -0,0 +1,13 @@
+// use super::lib::Res;
+// use nom::{
+//     character::complete::{char, i64},
+//     multi::separated_list1,
+// };
+
+// fn parse_(input: &str) -> Res<&str, _> {
+//     let (input, result) = separated_list1(char(','), i64)(input)?;
+// }
+
+pub fn run() {
+    println!("{:?}", "day 8");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 // mod day1;
 // mod day2;
 // mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
+// mod day4;
+// mod day5;
+// mod day6;
+// mod day7;
+mod day8;
+
 mod lib;
-mod nalgebra_testing;
+// mod nalgebra_testing;
 
 // This could be useful for day 5
 fn safe_convert(x: &f64) -> Option<isize> {
@@ -18,14 +20,15 @@ fn safe_convert(x: &f64) -> Option<isize> {
 }
 
 fn main() {
+    assert_eq!(safe_convert(&1.4), None);
+    assert_eq!(safe_convert(&1.0), Some(1));
     // day1::run();
     // day2::run();
     // day3::run();
     // day4::run();
     // nalgebra_testing::run();
-    day5::run();
+    // day5::run();
     // day6::run();
     // day7::run();
-
-    println!("{:?}", safe_convert(&1.4));
+    day8::run();
 }
