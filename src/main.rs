b25678e6c442f8c408e3fb96bc1986a4f85bf39d
Barry <chiroptical@gmail.com>
2021-12-07 19:07:43 -0500

    Add safe float conversion function for day 5

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,15 @@ mod day7;
 mod lib;
 mod nalgebra_testing;
 
+// This could be useful for day 5
+fn safe_convert(x: &f64) -> Option<isize> {
+    if x.fract() == 0.0 {
+        Some(*x as isize)
+    } else {
+        None
+    }
+}
+
 fn main() {
     // day1::run();
     // day2::run();
@@ -17,4 +26,6 @@ fn main() {
     // day5::run();
     // day6::run();
     day7::run();
+
+    println!("{:?}", safe_convert(&1.4));
 }
